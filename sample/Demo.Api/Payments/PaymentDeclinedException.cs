aed89eb255c9b0614f068503dfb4ebd58c91506b
Rebeca.Pereira <Rebeca.Pereira@jalasoft.com>
2025-12-05 11:09:18 -0400

    Add custom error in client side

---
+++ sample/Demo.Api/Payments/PaymentDeclinedException.cs
@@ -0,0 +1,23 @@
+﻿using Errors.Abstractions;
+using System.Net;
+
+namespace Demo.Api.Payments;
+
+public sealed class PaymentDeclinedException : Exception, IAppError
+{
+    public PaymentDeclinedException(string? detail = null, bool isTransient = false, HttpStatusCode? preferredStatus = null)
+        : base(detail)
+    {
+        Detail = detail;
+        IsTransient = isTransient;
+        PreferredStatus = preferredStatus ?? HttpStatusCode.PaymentRequired;
+    }
+
+    public ErrorCode Code { get; } = new("payment_declined", "Payment was declined", typeUri: "https://httpstatuses.io/402");
+
+    public string? Detail { get; }
+
+    public bool IsTransient { get; }
+
+    public HttpStatusCode? PreferredStatus { get; }
+}
---
+++ sample/Demo.Api/Payments/PaymentDeclinedExceptionMapper.cs
@@ -0,0 +1,31 @@
+﻿using Errors.AspNetCore.Formatters;
+using Errors.AspNetCore.Mappers;
+using Errors.AspNetCore.Registry;
+using Errors.AspNetCore.Sanitization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Demo.Api.Payments;
+
+[ExceptionMapper(priority: 200, IsFallback = false)]
+public sealed class PaymentDeclinedExceptionMapper(IExceptionSanitizer sanitizer)
+    : ExceptionProblemDetailsMapper<PaymentDeclinedException>(sanitizer)
+{
+    protected override (HttpStatusCode, ProblemDetails) MapTyped(HttpContext ctx, PaymentDeclinedException ex)
+    {
+        var status = ex.PreferredStatus ?? HttpStatusCode.PaymentRequired;
+        var sanitized = Sanitizer.Sanitize(ctx, ex, ex.Detail, treatPreferredDetailAsSensitive: true);
+
+        var problem = ProblemDetailsBuilder.Create(ctx)
+            .WithType(ex.Code.TypeUri ?? "https://httpstatuses.io/402")
+            .WithTitle(ex.Code.Title)
+            .WithDetail(sanitized.Detail)
+            .WithStatus(status)
+            .WithInstance()
+            .WithCode(ex.Code.Code)
+            .WithTraceId()
+            .Build();
+
+        return (status, problem);
+    }
+}
